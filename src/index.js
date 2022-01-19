e3b47f1f0e3a0eaab3fef503c1344649ed3f8f18
OmarHaroon01 <omarharoon01@yahoo.com>
2022-01-20 01:49:00 +0600

    navbar

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Navbar from './Components/navbar';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>,
   document.getElementById('root')
 );

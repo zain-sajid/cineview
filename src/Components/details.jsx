2fb7ad2345923e9f09122ec5080dba0d8124f49c
Zain Sajid <zsajid.bese19seecs@seecs.edu.pk>
2022-02-09 03:48:43 +0500

    Fixed a bug in details.jsx

--- src/Components/details.jsx
+++ src/Components/details.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import Reviews from "./reviews";
 
 function Details() {
   const [movie, setMovie] = useState({});
